99fb67cb3d1af2ae42fd1e84d9ebc897ebd1a9df
Nicholas Gerade <nicholasgerade@gmail.com>
2023-05-22 18:32:02 -0300

    Finalizando Pag. Produtos

--- js/swiper.js
+++ js/swiper.js
@@ -10,11 +10,30 @@ var swiper = new Swiper(".slide-content", {
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
-        dynamicBullets: true, 
+        dynamicBullets: true,
     },
 
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
+
+    breakpoints:{
+    0: {
+        slidesPerView: 1,
+    },
+
+    520: {
+        slidesPerView: 1,        
+    },
+
+    950: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+    },
+
+    1400: {
+        slidesPerView: 3,
+    },
+},
 });
