034b1119592137b06f2ef95e935baa662055991a
Nicholas Gerade <nicholasgerade@gmail.com>
2023-06-01 17:17:22 -0300

    Share Finalizado

--- js/compartilhar.js
+++ js/compartilhar.js
@@ -11,11 +11,6 @@ twitter.href = `http://twitter.com/share?&url=${link}&text=${msg}&hashtags=javas
 const linkedIn = document.querySelector('.linkedin');
 linkedIn.href = `https://www.linkedin.com/sharing/share-offsite/?url=${link}`;
 
-const reddit = document.querySelector('.reddit');
-reddit.href = `http://www.reddit.com/submit?url=${link}&title=${title}`;
-
 const whatsapp = document.querySelector('.whatsapp');
 whatsapp.href = `https://api.whatsapp.com/send?text=${msg}: ${link}`;
 
-const telegram = document.querySelector('.telegram');
-telegram.href = `https://telegram.me/share/url?url=${link}&text=${msg}`;
